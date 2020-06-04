453df1f8d25eda519f04a3f09ff2b35ef133248b
freedomdave <594144517@qq.com>
2020-06-04 09:29:17 +0800

    fluter:修改模板

--- src/app.js
+++ src/app.js
@@ -10,27 +10,28 @@ const index = require('./routes/index')
 const users = require('./routes/users')
 
 // error handler
-onerror(app)
+onerror(app)  // 在页面中显示错误信息
 
 // middlewares
 app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
 }))
 app.use(json())
-app.use(logger())
+app.use(logger())  // 日志
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
-  map: {"html":'nunjucks'}
+  map: {html:'nunjucks'}
+  // extension: "html"
 }))
 
 // logger 日志
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+// app.use(async (ctx, next) => {
+//   const start = new Date()
+//   await next()
+//   const ms = new Date() - start
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// })
 
 // routes
 app.use(index.routes(), index.allowedMethods())
@@ -38,7 +39,7 @@ app.use(users.routes(), users.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+  console.error('server error', err, ctx)   // 后台打印
 });
 
 module.exports = app
